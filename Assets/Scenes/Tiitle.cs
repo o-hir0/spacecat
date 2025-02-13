2457f114729011aa96dd567d8faba231d7083047
hir0 <katuyo1616.use@gmail.com>
2025-02-14 01:32:02 +0900

    Revert "feat:ファイル整理とTitleSceneに機能追加"
    This reverts commit de6bc77316653e45c683bf27c3a7dc2d3bf02937.

---
+++ Assets/Scenes/Tiitle.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Debug.Log("enter");
+            Invoke("ChangeScene", 1.0f);
+        }
+    }
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+}
