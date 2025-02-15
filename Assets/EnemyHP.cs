32a63cb00ea04e6bc0749d9f118c28baa3bc1634
kazz-1817 <kazumaxiaoguan@gmail.com>
2025-02-15 23:39:16 +0900

    ボス戦実装

--- Assets/EnemyHP.cs
+++ Assets/EnemyHP.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; // TextMeshPro用の名前空間を追加
 
 public class EnemyHP : MonoBehaviour
 {
-    public float HP = 100f;
+    public int HP = 100;
+    public int MaxHp;
+    public Slider HPBar;
+    public TMP_Text hpText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        MaxHp = 500;
+        HP = MaxHp;
+        //スライダーの最大値を変更
+        HPBar.maxValue = MaxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
+        HPBar.value = HP;
+
+        hpText.text = HP.ToString() + "/" + MaxHp.ToString();
         if (HP <= 0)
         {
             Destroy(gameObject);
